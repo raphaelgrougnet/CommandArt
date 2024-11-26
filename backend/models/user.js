1d5b9ab80375533354976a671f680e930526c388
Raphaël Grougnet <ragro001@edu.cegepgarneau.ca>
2024-11-25 20:48:06 -0500

    Create User model, migration and seed

---
+++ backend/models/user.js
@@ -0,0 +1,49 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate(models) {
+    }
+    toJSON() {
+      return { ...this.get(), password: undefined };
+
+    }
+  }
+  User.init({
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      defaultValue: null
+    },
+    isAdmin: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
---
+++ backend/seeders/20241125203535-seed-user.js
@@ -0,0 +1,63 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up (queryInterface, Sequelize) {
+        await queryInterface.bulkInsert('Users', [
+            {
+            id: 'a815906d-7056-4869-aa4b-f83d4cf668cc',
+            username: 'user_a',
+            email: 'a@asp.ca',
+            password: '$2a$12$yCPQpr9OvET25/FE4zuLcOasrxgOxtDDYefzCTsiPbWo5TeLJ.x.m', // password: 'G@rn3@u!'
+            address: '123 Main St',
+            isAdmin: true,
+            createdAt: new Date(),
+            updatedAt: new Date()
+            },
+            {
+              id: 'a815906d-7056-4869-aa4b-f83d4cf668cd',
+              username: 'user_b',
+              email: 'b@asp.ca',
+              password: '$2a$12$yCPQpr9OvET25/FE4zuLcOasrxgOxtDDYefzCTsiPbWo5TeLJ.x.m', // password: 'G@rn3@u!'
+              address: '123 Main St',
+              isAdmin: false,
+              createdAt: new Date(),
+              updatedAt: new Date()
+            },
+            {
+                id: 'a815906d-7056-4869-aa4b-f83d4cf668ce',
+                username: 'user_c',
+                email: 'c@asp.ca',
+                password: '$2a$12$yCPQpr9OvET25/FE4zuLcOasrxgOxtDDYefzCTsiPbWo5TeLJ.x.m', // password: 'G@rn3@u!'
+                address: '123 Main St',
+                isAdmin: false,
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                id: 'a815906d-7056-4869-aa4b-f83d4cf668cf',
+                username: 'user_d',
+                email: 'd@asp.ca',
+                password: '$2a$12$yCPQpr9OvET25/FE4zuLcOasrxgOxtDDYefzCTsiPbWo5TeLJ.x.m', // password: 'G@rn3@u!'
+                address: '123 Main St',
+                isAdmin: false,
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                id: 'a815906d-7056-4869-aa4b-f83d4cf668cg',
+                username: 'user_e',
+                email: 'e@asp.ca',
+                password: '$2a$12$yCPQpr9OvET25/FE4zuLcOasrxgOxtDDYefzCTsiPbWo5TeLJ.x.m', // password: 'G@rn3@u!'
+                address: '123 Main St',
+                isAdmin: false,
+                createdAt: new Date(),
+                updatedAt: new Date()
+            }
+        ], {});
+    },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Users', null, {});
+  }
+};
