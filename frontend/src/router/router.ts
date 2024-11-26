07b302b33b8051199f95b2914f8a3a71e631360c
Raphaël Grougnet <ragro001@edu.cegepgarneau.ca>
2024-11-26 17:02:10 -0500

    Add categories and logout routes

--- frontend/src/router/router.ts
+++ frontend/src/router/router.ts
@@ -1,4 +1,5 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, type RouteRecordRaw} from 'vue-router'
+import {useAuthStore} from "@/stores/authStore";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,12 +13,55 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: () => import("@/views/LoginView.vue"),
+      beforeEnter: (to, from, next) => {
+        const authStore = useAuthStore();
+
+        if (authStore.isLogged) {
+          return next("/")
+        }
+        return next()
+      }
     },
     {
       path: '/register',
       name: 'register',
       component: () => import("@/views/RegisterView.vue"),
-    }
+      beforeEnter: (to, from, next) => {
+        const authStore = useAuthStore();
+
+        if (authStore.isLogged) {
+          return next("/")
+        }
+        return next()
+      }
+    },
+    {
+      path: '/admin/categories',
+      name: 'categories',
+      component: () => import("@/views/admin/CategoriesView.vue"),
+      beforeEnter: async (to, from, next) => {
+        const authStore = useAuthStore();
+
+        if (!authStore.isLogged) {
+          return next("/login")
+        }
+        if (!authStore.currentUser?.isAdmin) {
+          return next("/")
+        }
+        await authStore.getCurrentUser()
+        return next()
+      }
+    },
+    {
+      path: '/logout',
+      name: 'logout',
+      component: { template: '<div></div>' },
+      beforeEnter: async (to, from, next) => {
+        const authStore = useAuthStore();
+        await authStore.logout();
+        return next({name : 'login'});
+      },
+    } as RouteRecordRaw
   ],
 })
 
