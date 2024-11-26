25ea402ca5d850ca67c05dfd95d518947b4e02f2
Raphaël Grougnet <ragro001@edu.cegepgarneau.ca>
2024-11-26 17:05:16 -0500

    Create homeview

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,42 @@
 <script setup lang="ts">
+  import {useCategoryStore} from "@/stores/categoryStore";
+  import {getEmitter} from "@/stores/authStore";
+  import {computed, onMounted, onUnmounted} from "vue";
+  import router from "@/router/router";
+  import Navbar from "@/components/Navbar.vue";
+  import type {NavbarLinks} from "@/types/NavbarLinks";
 
+  const emitter = getEmitter();
+
+  onMounted(() => {
+    emitter.on('navigate', navigate);
+  });
+
+  onUnmounted(() => {
+    emitter.off('navigate', navigate);
+  });
+
+  const navigate = (route: any) => {
+    router.push(route);
+  }
+
+  const links: NavbarLinks[] = [
+    {
+      name: 'Accueil',
+      route: 'home',
+      isActive: true
+    },
+    {
+      name: 'Catégories',
+      route: 'categories',
+      isActive: false,
+      isForAdmin: true
+    }
+  ]
 </script>
 
 <template>
-  <p>This is home view</p>
+  <Navbar :links="links"/>
 </template>
 
 <style scoped>
